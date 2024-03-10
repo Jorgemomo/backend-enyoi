41f84ebcc3c588c2ba86089d21ef029913baa9d4
Jorgemomo <jmomo581@gmail.com>
2024-03-10 14:35:09 -0500

    Uso de libreria cors para integración

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express"); // se llama  al modulo de express
 const routers = require("./router/index"); // importamos los rutas
+const cors = require("cors");
 
 const app = express(); //se instancia  el servidor con la clase Express
 
 app.use(express.json()); //permite que las peticiones json sean interpretadas por express
+
+app.use(cors()); // uso de cors
 
 routers(app); //se instancia las rutas
 
